317bd3a06c36213206cba1e34489fc6cc20053d4
Abs1n7he <wuxinyangliu@163.com>
2023-03-31 22:55:43 +0800

    2023/03/31

--- module/color.py
+++ module/color.py
@@ -4,10 +4,12 @@ from colorama import init
 from colorama import Fore, Back, Style, Cursor
 init(autoreset=True)
 
-#Fore是针对字体颜色，Back是针对字体背景颜色，Style是针对字体格式
-#Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-#Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
-#Style: DIM 暗, NORMAL 正常, BRIGHT 亮, RESET_ALL
+#Style字体格式: DIM 暗, NORMAL 正常, BRIGHT 亮, RESET_ALL
+
+#Fore字体颜色: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+
+#Back背景颜色: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+
 class Colored:
     @staticmethod
     def cyan(s):
@@ -19,22 +21,7 @@ class Colored:
     def red(s):
         return Style.BRIGHT + Fore.RED     + s + Fore.RESET + Style.RESET_ALL
     @staticmethod
-    def red_twinkle(s):
-        return Style.twinkle + Fore.RED     + s + Fore.RESET + Style.RESET_ALL
-
-    @staticmethod
     def green(s):
-        return Style.BRIGHT + Fore.GREEN+ s +   Fore.RESET + Style.RESET_ALL
-    @staticmethod
-    def white(s):
-        return Fore.WHITE + s +                Fore.RESET + Style.RESET_ALL
-
-    @staticmethod
-    def cyan_fine(s):
-        return Fore.CYAN + s +                 Fore.RESET + Style.RESET_ALL
-    @staticmethod
-    def yellow(s):
-        return Style.BRIGHT + Fore.YELLOW + s +  Fore.RESET + Style.RESET_ALL
-
+        return Style.BRIGHT + Fore.GREEN   + s + Fore.RESET + Style.RESET_ALL
         
 color = Colored()
